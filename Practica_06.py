8dff191ffc0ddb22f63b9f9866c460abdcbefefb
sergioramirez-11 <ramirezhernandezsergio18@gmail.com>
2024-09-02 23:45:33 -0600

    Add files via upload

---
+++ Practica_06.py
@@ -0,0 +1,11 @@
+#crear lista inicial
+mi_lista = [1, 2, 3, 4, 5]
+
+#agregar un elemento al final
+mi_lista.append(6)
+
+#insertar un elemento en la posicion 2
+mi_lista.insert(2, 10)
+
+#insertar la lista actualizada
+print(mi_lista)
